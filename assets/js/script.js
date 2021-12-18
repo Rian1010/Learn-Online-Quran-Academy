fd0eee68347eacabb2bee89d10ef524c8d4c48f1
Rian Sunthbocus <rian@rians-mbp.speedport.ip>
2021-12-18 18:05:07 +0100

    Refactoring the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,10 +37,28 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
-            const courses = data.homepage.courses
+
+            const banner = data.homepage.banner;
+            console.log(banner);
+            banner.heading.forEach(heading => {
+                document.getElementById('bannerTitle').innerHTML += `
+                    ${heading}<br />
+                `;
+            })
+            banner.bulletPoints.forEach(bulletPoint => {
+                document.getElementById('bannerList').innerHTML += `
+                <li class="p-2 no-banner-bullet-point">
+                    <i class="fas fa-check"></i>
+                    <span class="fs-5">${bulletPoint}</span>
+                </li>
+                `;
+            })
+
+            // Courses
+            const courses = data.homepage.courses;
             console.log(courses);
             courses.forEach(course => {
-                document.querySelector('#coursesRow').innerHTML += `
+                document.getElementById('coursesRow').innerHTML += `
                     <div class="col-lg-4 mb-4 d-flex justify-content-center">
                         <div class="card w-100" style="width: 18rem">
                         <img
@@ -55,9 +73,9 @@ document.addEventListener("DOMContentLoaded", function () {
                             </p>
                         </div>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
+                            <li class="list-group-item">${course.infoItems.item1}</li>
+                            <li class="list-group-item">${course.infoItems.item2}</li>
+                            <li class="list-group-item">${course.infoItems.item3}</li>
                         </ul>
                         <div class="card-body">
                             <a href="#" class="card-link">Card link</a>
@@ -66,6 +84,63 @@ document.addEventListener("DOMContentLoaded", function () {
                         </div>
                     </div>
                 `
-            })
+            });
+            // About Section
+            const aboutUs = data.homepage.aboutUs;
+            console.log(aboutUs);
+            document.getElementById('aboutUsTitle').innerHTML += aboutUs.title;
+
+            document.querySelector('.about-col-2').style.backgroundImage = `url(${aboutUs.images.imagePath1})`;
+            document.querySelector('.about-col-6').style.backgroundImage = `url(${aboutUs.images.imagePath2})`;
+
+            document.getElementById('aboutParagraph1').innerHTML += aboutUs.textContent.paragraph1;
+            document.getElementById('aboutParagraph2').innerHTML += aboutUs.textContent.paragraph2;
+            document.getElementById('aboutParagraph3').innerHTML += aboutUs.textContent.paragraph3;
+            document.getElementById('aboutParagraph4').innerHTML += aboutUs.textContent.paragraph4;
+
+
+
+            // Care
+            const care = data.homepage.care;
+            console.log(care);
+            document.getElementById('careTitle').innerHTML += care.title;
+            care.mainContent.forEach(content => {
+                document.getElementById('careContent').innerHTML += `<div
+                        class="
+                            col-md-4
+                            text-center
+                            h-100
+                            d-flex
+                            flex-column
+                            justify-content-center
+                            px-5
+                            pb-5
+                            "
+                        >
+                        <div class="benefits_section pb-3">
+                            ${content.icon}
+                        </div>
+                        <p>
+                            ${content.text}
+                        </p>
+                    </div>
+                `;
+            });
+
+
+
+            const demo = data.homepage.demo;
+            console.log(demo);
+            document.getElementById('demo').innerHTML += `
+                <div class="col-md-6 d-flex flex-column justify-content-center">
+                    <h2>${demo.title}</h2>
+                    <p>
+                        ${demo.quickDescription}
+                    </p>
+                </div>
+                <div class="col-md-6 d-flex justify-content-center">
+                    ${demo.video}
+                </div>
+            `;
         });
 });
