430ae904e8d15174af30d313516157a9bcda5069
Rian Sunthbocus <rian@rians-mbp.speedport.ip>
2021-12-23 20:14:42 +0100

    Trying to solve a grid layout bug

---
+++ assets/js/blog.js
@@ -0,0 +1,89 @@
+const card1 = document.getElementById('card1');
+const card2 = document.getElementById('card2');
+const card3 = document.getElementById('card3');
+const card4 = document.getElementById('card4');
+const card5 = document.getElementById('card5');
+const card6 = document.getElementById('card6');
+
+// const topCardBottomPos = card1.offsetTop + card1.offsetHeight;
+// const bottomCardTopPos = card4.offsetTop;
+// const distanceBetweenCards = bottomCardTopPos - topCardBottomPos
+
+// console.log(topCardBottomPos, bottomCardTopPos, distanceBetweenCards)
+// // card4.style.position = "absolute";
+// card4.style.marginTop = distanceBetweenCards / 2 + "px";
+
+// const cardBottomPositioning = (topCard, targetCard) => {
+//     if (topCard && targetCard) {
+//         const topCardBottomPos = topCard.offsetTop + topCard.offsetHeight;
+//         const bottomCardTopPos = targetCard.offsetTop;
+//         const distanceBetweenCards = bottomCardTopPos - topCardBottomPos
+
+//         console.log(topCardBottomPos, bottomCardTopPos, distanceBetweenCards)
+//         // card4.style.position = "absolute";
+
+//         if (screen.width >= 992) {
+//             targetCard.style.marginTop = -distanceBetweenCards + 25 + "px";
+//         }
+//     }
+// }
+
+// cardBottomPositioning(card1, card4);
+// cardBottomPositioning(card2, card5);
+// cardBottomPositioning(card3, card6);
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("./../../blogs.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const blogs = data.blogs;
+            blogs.forEach(blog => {
+                document.getElementById('blogCards').innerHTML += `
+                    <div class="col-lg-4 mt-5 card-column">
+                        <div class="card card-blog" style="width: 85%">
+                        <img
+                            src="${blog.image}"
+                            class="card-img-top"
+                            alt="..."
+                        />
+                        <div class="card-body card-body-blog">
+                            <h5 class="card-title">${blog.title}</h5>
+                            <p class="card-text">
+                                ${blog.shortDescription}
+                            </p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                `;
+            });
+
+            const cardCol = document.querySelectorAll('.card-column');
+            for (let i = 0; i < cardCol.length; i++) {
+                if (i == 0) {
+                    cardCol[i].classList.add('pt-3');
+                } else if (i == 1) {
+                    cardCol[i].classList.add('pt-5');
+                }
+            }
+            const cards = document.querySelectorAll('.card');
+            for (let i = 0; i < cards.length - 3; i++) {
+                const topCard = cards[i];
+                const targetCard = cards[i + 3];
+
+                const topCardBottomPos = topCard.offsetTop + topCard.clientHeight;
+                const bottomCardTopPos = targetCard.offsetTop;
+                const distanceBetweenCards = bottomCardTopPos - topCardBottomPos
+
+                console.log(topCardBottomPos, bottomCardTopPos, distanceBetweenCards)
+                // card4.style.position = "absolute";
+
+                if (screen.width >= 992) {
+
+                    targetCard.style.marginTop = -distanceBetweenCards + "px";
+                }
+            }
+
+        });
+
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-const navBg = document.querySelector('.primary-bg');
-const navTextColor = document.querySelectorAll('.nav-item-color');
-const navSearchBtn = document.getElementById('navSearchBtn');
-
-window.onscroll = () => navDesignOnScroll();
-
-function navDesignOnScroll() {
-    if (document.documentElement.scrollTop > 50) {
-        navBg.style.backgroundColor = "rgba( 255, 255, 255, 0.9 )";
-        navBg.style.boxShadow = "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )";
-        navBg.style.backdropFilter = "4px";
-        navBg.style.border = "1px solid rgba( 255, 255, 255, 0.18 )";
-
-        navTextColor.forEach(txt => txt.style.color = "#333");
-
-        if (navSearchBtn.classList.contains("btn-outline-light")) {
-            navSearchBtn.classList.remove("btn-outline-light")
-        }
-        navSearchBtn.classList.add("btn-outline-dark");
-    } else {
-        navTextColor.forEach(txt => txt.style.color = "#fff");
-
-        navBg.style.backgroundColor = "rgba( 0, 0, 0, 0)";
-        navBg.style.boxShadow = "0 0 0 0 rgba(0, 0, 0, 0)";
-        navBg.style.backdropFilter = "0px";
-        navBg.style.border = "0";
-
-        if (navSearchBtn.classList.contains("btn-outline-dark")) {
-            navSearchBtn.classList.remove("btn-outline-dark")
-        }
-        navSearchBtn.classList.add("btn-outline-light")
-    }
-}
-
 const carouselItems = document.querySelectorAll('.carousel-item');
 
 // carouselItems.forEach(item => {
@@ -42,8 +8,8 @@ const carouselItems = document.querySelectorAll('.carousel-item');
 // });
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("../content.json")
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("./../../content.json")
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
